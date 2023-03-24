f47ff09d75c7fb71a8eda12a59dae79c4497ab48
liangleicn <120149129+liangleicn@users.noreply.github.com>
2023-03-24 23:02:23 +0800

    Update todayrunexample.py

--- todayrunexample.py
+++ todayrunexample.py
@@ -1,24 +1,25 @@
 import sqlite3
-import datetime
+from datetime import datetime, timedelta
 
-# Connect to the SQLite database
+# Connect to the database
 conn = sqlite3.connect('mydatabase.db')
-cursor = conn.cursor()
+c = conn.cursor()
 
-# Get the current date and time in the same format as the boot_time column
-now = datetime.datetime.now().strftime('%Y%m%d%H')
+# Define the time window to check
+end_date = datetime.now()
+start_date = end_date - timedelta(days=7)
 
-# Execute the query to retrieve rows where boot_time is now
-cursor.execute("SELECT user_name FROM mytable WHERE boot_time = ?", (now,))
-rows = cursor.fetchall()
+# Define the exclude lists
+machine_list_name = ['machine1', 'machine2', 'machine3']
+developer_list_name = ['dev1', 'dev2', 'dev3']
+already_involved_list_name = ['user1', 'user2', 'user3']
 
-# Print the results
-if len(rows) > 0:
-    print("The following users have booted up now:")
-    for row in rows:
-        print(row[0])
-else:
-    print("No users have booted up now.")
+# Query the database
+c.execute("SELECT DISTINCT user_name FROM mytable WHERE boot_time >= ? AND boot_time <= ?", (start_date.strftime("%Y%m%d"), end_date.strftime("%Y%m%d")))
+users = [row[0] for row in c.fetchall() if row[0] not in machine_list_name and row[0] not in developer_list_name and row[0] not in already_involved_list_name]
 
-# Close the database connection
+# Print the resulting user list
+print(users)
+
+# Close the connection to the database
 conn.close()
