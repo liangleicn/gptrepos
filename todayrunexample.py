03c7f29cc41c69eb9c2a7683ca8a3a0d3fb84918
liangleicn <120149129+liangleicn@users.noreply.github.com>
2023-03-24 22:55:38 +0800

    Update todayrunexample.py

--- todayrunexample.py
+++ todayrunexample.py
@@ -5,20 +5,20 @@ import datetime
 conn = sqlite3.connect('mydatabase.db')
 cursor = conn.cursor()
 
-# Get today's date in the same format as the boot_time column
-today = datetime.datetime.now().strftime('%Y%m%d')
+# Get the current date and time in the same format as the boot_time column
+now = datetime.datetime.now().strftime('%Y%m%d%H')
 
-# Execute the query to retrieve rows where boot_time is today
-cursor.execute("SELECT user_name FROM mytable WHERE boot_time = ?", (today,))
+# Execute the query to retrieve rows where boot_time is now
+cursor.execute("SELECT user_name FROM mytable WHERE boot_time = ?", (now,))
 rows = cursor.fetchall()
 
 # Print the results
 if len(rows) > 0:
-    print("The following users have booted up today:")
+    print("The following users have booted up now:")
     for row in rows:
         print(row[0])
 else:
-    print("No users have booted up today.")
+    print("No users have booted up now.")
 
 # Close the database connection
 conn.close()
